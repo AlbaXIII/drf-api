74223c5e0e524fe99d30a20cc5d76fcdac17510e
AlbaXIII <alex.jbaker20@googlemail.com>
2025-04-02 12:16:16 +0100

    fixed allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'drf-api-albadev.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'drf-api-albadev-0e5d6dc78b06.herokuapp.com']
 
 
 # Application definition
