a0ba7656fb32359bda549b6af8883247b59b1a7f
AlbaXIII <alex.jbaker20@googlemail.com>
2025-04-02 12:07:29 +0100

    prep for heroku

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://475832927785242:z5dTKIGQXHADaAx4rKLDnPeeCG8@di3x6qyfa'
+os.environ.setdefault("SECRET_KEY", "jexsbLAVccHDJjcdCjZkiQc6FsaoNK7u")
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:kfYTneo4Qw9A@ep-young-fog-a2ic5c75.eu-central-1.aws.neon.tech/shed_opal_verse_433619"
